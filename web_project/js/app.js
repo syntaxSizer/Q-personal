afbcdcb2d7117fde3394a23ac4035f92bf32121e
syntaxSizer <kakuma4never@gmail.com>
2016-04-12 16:32:02 +0300

    add scroll delay effect

--- web_project/js/app.js
+++ web_project/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+$('a[href^="#"]').on('click', function (event) {
+
+    var target = $($(this).attr('href'));
+
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
